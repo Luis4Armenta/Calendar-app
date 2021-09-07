d847352ebbd5105d7230c5de8c5e0b54fc8ee739
beku86 <luigiangelxd@gmail.com>
2021-09-07 13:01:05 -0500

    HashRouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Switch, Redirect } from 'react-router-dom'
 import { startChecking } from '../actions/auth'
 import { LoginScreen } from '../components/auth/LoginScreen'
 import { CalendarScreen } from '../components/calendar/CalendarScreen'
